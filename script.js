5cc1655b7520daaebfa2d4e52912600fd4992f9b
gefgu <gefgu@hotmail.com>
2022-01-23 09:41:48 -0300

    Add game objects pseudo functionality

---
+++ script.js
@@ -0,0 +1,16 @@
+// gameBoard
+// stores the board
+// check for wins or ties
+
+// displayController
+// who can play
+// where the mark can be placed
+// sends to gameboard the marks
+// display the board
+// display the messages
+
+
+// player
+// stores the name
+// stores the mark
+
