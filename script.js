13afaebdfa0deae2dac2dc1d285c56776cd067f0
gefgu <gefgu@hotmail.com>
2022-01-23 10:09:00 -0300

    Add displayController function to create gameBoard html

--- script.js
+++ script.js
@@ -18,8 +18,33 @@ const gameBoard = (() => {
 // display the board
 // display the messages
 // restart the game
-
+const displayController = ((doc) => {
+  const _createThreeDivs = () => {
+    return [
+      doc.createElement("div"),
+      doc.createElement("div"),
+      doc.createElement("div"),
+    ];
+  };
+  const createBoard = () => {
+    const container = doc.querySelector(".container");
+    const board = doc.createElement("div");
+    board.classList.add("board")
+    board.append(..._createThreeDivs());
+    board.childNodes.forEach((div, rowIndex) => {
+        _createThreeDivs().forEach((positionDiv, colIndex) => {
+            positionDiv.dataset.position = (rowIndex * 3) + colIndex;
+            positionDiv.classList.add("spot");
+            div.appendChild(positionDiv);
+        });
+    });
+    container.appendChild(board);
+  };
+  return {createBoard};
+})(document);
 
 // player
 // stores the name
 // stores the mark
+
+displayController.createBoard();
